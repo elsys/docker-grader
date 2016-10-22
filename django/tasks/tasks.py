df9a82dee75974e602cccd06c9b8c915ee4dacbe
Nikolay Dimitrov <nikolay.dimitrov@viblast.com>
2016-10-22 16:11:12 +0300

    Grading task now works with models

--- django/tasks/tasks.py
+++ django/tasks/tasks.py
@@ -2,13 +2,18 @@ from __future__ import absolute_import
 
 from celery import shared_task
 from docker import Client
+from tasks.models import TaskSubmission
 
 
 @shared_task
-def grade(filename):
-    runner = TaskRunner("gcc:latest", filename)
-    runner.exec_step("gcc -x c /mnt/input")
-    res = runner.exec_step("./a.out")
+def grade(submission_id):
+    submission = TaskSubmission.objects.get(pk=submission_id)
+
+    runner = TaskRunner(submission.task.docker_image, submission.get_submission_path())
+    res = ""
+    for step in submission.task.steps.order_by("order"):
+        res = runner.exec_step(step.input_source)
+        print(res)
     return res
 
 
--- django/tasks/tests/test_tasks.py
+++ django/tasks/tests/test_tasks.py
@@ -1,19 +1,37 @@
+import os
+import pytest
+
 from tasks.tasks import grade, TaskRunner
 from django.test.utils import override_settings
-import os
+from tasks.models import Task, TaskSubmission, TaskStep
+from authentication.models import User
+from shutil import copyfile
+
+
+HELLO_C_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "hello.c")
 
 
 def test_docker():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-
-    runner = TaskRunner("gcc:latest", dir_path + "/hello.c")
+    runner = TaskRunner("gcc:latest", HELLO_C_PATH)
     runner.exec_step("gcc -x c /mnt/input")
     result = runner.exec_step("./a.out")
     assert result.decode("utf8") == "hello world\n"
 
 
+@pytest.mark.django_db
 @override_settings(CELERY_ALWAYS_EAGER=True)
 def test_queue():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    result = grade.delay(dir_path + "/hello.c")
+    task = Task.objects.create(docker_image="gcc:latest", slug="test_task")
+    user = User.objects.create()
+    TaskStep.objects.create(task=task, input_source="gcc -x c /mnt/input", order=1)
+    TaskStep.objects.create(task=task, input_source="./a.out", order=2)
+    taskSubmission = TaskSubmission.objects.create(task=task, user=user)
+
+    if not os.path.exists(task.get_task_dir()):
+        os.mkdir(task.get_task_dir())
+    copyfile(HELLO_C_PATH, taskSubmission.get_submission_path())
+
+    result = grade.delay(taskSubmission.id)
+
+    os.remove(taskSubmission.get_submission_path())
     assert result.get().decode("utf8") == "hello world\n"
