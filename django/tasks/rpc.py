eb5e084c85ea93363919433ab25c129e2cfa8f72
Nikolay Dimitrov <nikidimi@gmail.com>
2016-10-26 02:42:40 +0300

    Added default value for state in grading steps

--- django/tasks/rpc.py
+++ django/tasks/rpc.py
@@ -2,13 +2,13 @@ from xmlrpc.server import SimpleXMLRPCServer
 
 
 def prepare_input_command(src, state):
-    scope = {"state_in": state}
+    scope = {"state_in": state, "state_out": None}
     exec(src, scope, scope)
     return {"command": scope["command"], "state": scope["state_out"]}
 
 
 def parse_output(src, state, stdout, stderr, status_code):
-    scope = {"state_in": state, "stdout": stdout, "stderr": stderr, "status_code": status_code}
+    scope = {"state_in": state, "stdout": stdout, "stderr": stderr, "status_code": status_code, "state_out": None}
     exec(src, scope, scope)
     return {"state": scope["state_out"],
             "grade": scope["grade"],
--- django/tasks/tests/test_rpc.py
+++ django/tasks/tests/test_rpc.py
@@ -26,3 +26,27 @@ def test_parse_output():
     assert result["grade"] == 30
     assert result["output_msg"] == "blabla"
     assert result["exec_next_step"]
+
+
+TEST_INPUT_NO_STATE = """
+command = "blabla"
+"""
+
+
+def test_prepare_input_command_no_state():
+    result = rpc.prepare_input_command(TEST_INPUT_NO_STATE, 1)
+    assert result["command"] == "blabla"
+
+
+TEST_OUTPUT_NO_STATE_SOURCE = """
+output_msg = "blabla"
+grade = state_in * status_code
+exec_next_step = True
+"""
+
+
+def test_parse_output_no_state():
+    result = rpc.parse_output(TEST_OUTPUT_NO_STATE_SOURCE, 2, "stdout", "stderr", 15)
+    assert result["grade"] == 30
+    assert result["output_msg"] == "blabla"
+    assert result["exec_next_step"]
