747552512694963da5b1a0ab652ab2ecf27fe9c6
Nikolay Dimitrov <nikidimi@gmail.com>
2016-10-27 20:53:15 +0300

    Renamed upload file label in UI to File

--- django/tasks/forms.py
+++ django/tasks/forms.py
@@ -3,4 +3,4 @@ from django import forms
 
 class TaskForm(forms.Form):
     zip_file = forms.FileField(required=True, allow_empty_file=False,
-                               max_length=100)
+                               max_length=100, label="File")
