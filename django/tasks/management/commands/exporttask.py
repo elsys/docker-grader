84a90816c07a5106a1e73774231826909cd112c3
Nikolay Dimitrov <nikidimi@gmail.com>
2017-04-28 01:52:15 +0300

    Custom export format for tasks added

--- django/tasks/management/commands/exporttask.py
+++ django/tasks/management/commands/exporttask.py
@@ -3,30 +3,18 @@ from tasks.models import Task
 from lxml import etree
 
 class Command(BaseCommand):
-    help = 'Dumps task into file filename.yaml'
+    help = 'Dumps task into file filename'
     args = '<task-slug> <filename>'
 
-
     def prettify_source(self, source):
-        return "\n" + source.replace("\r\n","\n") + "\n"
-
-    def prepare_step(self, step, step_el):
-        etree.SubElement(step_el, "input").text = self.prettify_source(step.input_source)
-        etree.SubElement(step_el, "output").text = self.prettify_source(step.output_source)
+        return "\n" + source.replace("\r\n","\n").strip() + "\n"
 
     def handle(self, *args, **options):
         task = Task.objects.get(slug=args[0])
 
-        task_el = etree.Element("task")
-        etree.SubElement(task_el, "id").text=task.slug
-        etree.SubElement(task_el, "docker_image").text=task.docker_image
-        etree.SubElement(task_el, "description").text=task.description
-
-        steps_el = etree.SubElement(task_el, "steps")
-
-        for step in task.steps.order_by("order"):
-            step_el = etree.SubElement(steps_el, "step")
-            self.prepare_step(step, step_el)
-
-        tree = etree.ElementTree(task_el)
-        tree.write(args[1], pretty_print = True)
+        with open(args[1], 'w') as outfile:
+            for step in task.steps.order_by("order"):
+                outfile.write("#-----INPUT-TASK-STEP-----")
+                outfile.write(self.prettify_source(step.input_source))
+                outfile.write("#-----OUTPUT-TASK-STEP-----")
+                outfile.write(self.prettify_source(step.output_source))
