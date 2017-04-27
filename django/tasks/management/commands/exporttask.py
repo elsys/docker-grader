9908617053e830e3a5b46868fe2bbc3a45d9535a
Nikolay Dimitrov <nikidimi@gmail.com>
2017-04-27 22:58:01 +0300

    Use XML format instead of YAML for export

--- django/tasks/management/commands/exporttask.py
+++ django/tasks/management/commands/exporttask.py
@@ -1,35 +1,29 @@
 from django.core.management.base import BaseCommand, CommandError
 from tasks.models import Task
-import yaml
-
-class literal_str(str): pass
-
-def literal_str_representer(dumper, data):
-    return dumper.represent_scalar(u'tag:yaml.org,2002:str', data, style='|')
-
-yaml.add_representer(literal_str, literal_str_representer)
+from lxml import etree
 
 class Command(BaseCommand):
     help = 'Dumps task into file filename.yaml'
     args = '<task-slug> <filename>'
 
 
-    def prepare_step(self, step):
-        return {
-            "input_source": literal_str(step.input_source),
-            "output_source": literal_str(step.output_source)
-        }
+    def prepare_step(self, step, step_el):
+        etree.SubElement(step_el, "input").text = step.input_source
+        etree.SubElement(step_el, "output").text = step.output_source
 
     def handle(self, *args, **options):
         task = Task.objects.get(slug=args[0])
-        steps = [self.prepare_step(step) for step in task.steps.order_by("order")]
 
-        task_dict = {
-            "id": task.slug,
-            "docker_image": task.docker_image,
-            "description": task.description,
-            "steps": steps
-        }
+        task_el = etree.Element("task")
+        etree.SubElement(task_el, "id").text=task.slug
+        etree.SubElement(task_el, "docker_image").text=task.docker_image
+        etree.SubElement(task_el, "description").text=task.description
 
-        with open(args[1], 'w') as stream:
-            yaml.dump(task_dict, stream)
+        steps_el = etree.SubElement(task_el, "steps")
+
+        for step in task.steps.order_by("order"):
+            step_el = etree.SubElement(steps_el, "step")
+            self.prepare_step(step, step_el)
+
+        tree = etree.ElementTree(task_el)
+        tree.write(args[1], pretty_print = True)
