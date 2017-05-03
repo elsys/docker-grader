486b543f9576955a755c6f748d33e78174edf0eb
Nikolay Dimitrov <nikidimi@gmail.com>
2017-05-04 00:31:09 +0300

    Added command for importing tasks

---
+++ django/tasks/management/commands/importtask.py
@@ -0,0 +1,35 @@
+from django.core.management.base import BaseCommand, CommandError
+from tasks.models import Task, TaskStep
+from lxml import etree
+
+class Command(BaseCommand):
+    help = 'Dumps task into file filename'
+    args = '<task-slug> <docker-image> <filename>' 
+
+    def handle(self, *args, **options):
+        inputs = []
+        outputs = []
+
+        with open(args[2], 'r') as infile:
+            currentBuffer = ""
+
+
+            for line in infile:
+                if line.strip() == "#-----INPUT-TASK-STEP-----":
+                    if currentBuffer != "":
+                        outputs.append(currentBuffer)
+                    currentBuffer = ""
+                elif line.strip() == "#-----OUTPUT-TASK-STEP-----":
+                    if currentBuffer != "":
+                        inputs.append(currentBuffer)
+                    currentBuffer = ""
+                else:
+                    currentBuffer += line
+
+            if currentBuffer != "":
+                outputs.append(currentBuffer)
+
+        task = Task.objects.create(slug=args[0], docker_image=args[1])
+        for i, (input_code, output_code) in enumerate(zip(inputs, outputs)):
+            TaskStep.objects.create(order=i, task=task, 
+                                    input_source=input_code, output_source=output_code)
