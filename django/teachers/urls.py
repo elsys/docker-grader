6aebcffb6111c7ab91bf18b1e70935cc1e0e6c72
Nikolay Dimitrov <nikidimi@gmail.com>
2019-10-12 18:38:41 +0300

    Students can now download their submissions

--- django/teachers/urls.py
+++ django/teachers/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
         SubmissionsDataView.as_view(),
         name='task-submissions'),
     url(r'^download/(?P<submission_id>\d+)/$', views.download, name='download'),
+    url(r'^download_mine/(?P<submission_id>\d+)/$', views.download_mine, name='download_mine'),
     url(r'^download_all/(?P<task_id>\d+)/$', views.download_all_for_task, name='download_all_for_task'),
 ]
